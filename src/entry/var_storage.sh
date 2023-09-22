c564a2d245ff06b6c70e8a127d40913f5b948fde
Angela <144486389+amshipman117@users.noreply.github.com>
2023-09-22 15:04:57 -0400

    Update var_storage.sh

--- src/entry/var_storage.sh
+++ src/entry/var_storage.sh
@@ -1,25 +1,27 @@
 #USER DEFINED VARIABLES
 
-export user_directory='/update/me/with/your/path/'
-
-if [ $hostsystem = 'chem' ]
-then
-    export user_directory="/home/$USER"
-elif [ $hostsystem = 'bridges2' ]
-then
-    export user_directory="/ocean/projects/che220043p/$USER"
-elif [ $hostsystem = 'hazel' ]
-then
-    export user_directory="/share/ssp/$USER"
-fi
-
 #This sets the directory that ae.sh will search for ALL entries you've ever made on the machine. On your local computer, this can be '~' to search the entire computer. For an HPC, you'll likely want to set this to your user directory.
 #Common User Directories:
 #Hazel: '/share/ssp/YourUnityID'
 #Bridges: '/ocean/projects/che220043p/YourUsername'
+export user_directory='/update/me/with/your/path/'
 
-export index="-L"
 #The index gives unique identifiers to the names of your entries. For example, I use "L" for entries created on my local computer, "H" on Hazel, and "B" on Bridges. You may want to use one based on project, material, etc... do what suits you! Leave it blank if you don't want an index at all.
+export index="-L"
+
+if [ $hostsystem = 'chem' ]
+then
+    export user_directory="/home/$USER"
+    export index="-L"
+elif [ $hostsystem = 'bridges2' ]
+then
+    export user_directory="/ocean/projects/che220043p/$USER"
+    export index="-B"
+elif [ $hostsystem = 'hazel' ]
+then
+    export user_directory="/share/ssp/$USER"
+    export index="-H"
+fi
 
 #AUTO-GENERATED (No need to touch!)
 export default_material=''
